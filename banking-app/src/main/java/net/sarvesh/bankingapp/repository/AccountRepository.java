c4e2736292796ad5bf3058cb8086b298de18200f
Sarvesh Yeutkar <98184185+saarv3sh7@users.noreply.github.com>
2026-03-31 15:02:58 +0530

    Delete banking-app directory

--- banking-app/src/main/java/net/sarvesh/bankingapp/repository/AccountRepository.java
+++
@@ -1,8 +0,0 @@
-package net.sarvesh.bankingapp.repository;
-
-import net.sarvesh.bankingapp.entity.Account;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<Account, Long> {
-
-}
