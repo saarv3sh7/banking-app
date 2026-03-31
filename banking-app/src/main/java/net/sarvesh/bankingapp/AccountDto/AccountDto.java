35341ec2256992dd3288503d14f058f2de77c996
Sarvesh Yeutkar <98184185+saarv3sh7@users.noreply.github.com>
2026-03-31 15:02:11 +0530

    Add files via upload

---
+++ banking-app/src/main/java/net/sarvesh/bankingapp/AccountDto/AccountDto.java
@@ -0,0 +1,12 @@
+package net.sarvesh.bankingapp.AccountDto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AccountDto {
+    private Long id;
+    private String accountHolderName;
+    private double balance;
+}
